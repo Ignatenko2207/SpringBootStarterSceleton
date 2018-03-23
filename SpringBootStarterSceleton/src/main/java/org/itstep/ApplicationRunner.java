ca516fa35f087ce04581d1ead69ff65ee19807ed
Alex Ignatenko <alex.ignatenko@scalors.com>
2018-03-23 20:58:36 +0200

    Made sceleton

--- SpringBootStarterSceleton/src/main/java/org/itstep/ApplicationRunner.java
+++ SpringBootStarterSceleton/src/main/java/org/itstep/ApplicationRunner.java
@@ -1,14 +1,18 @@
 package org.itstep;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ApplicationRunner 
 {
+	
     public static void main( String[] args )
     {
     	ApplicationContext ctx = SpringApplication.run(ApplicationRunner.class, args);
+    
     }
 }
